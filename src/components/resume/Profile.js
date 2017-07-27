1a733f258aa8621fb59b244edb9cb7ee43ad01cd
Ed <ultralabed@gmail.com>
2017-07-27 20:13:50 +0800

    feat: profile page data from profile.json

--- src/components/resume/Profile.js
+++ src/components/resume/Profile.js
@@ -1,45 +1,43 @@
 import React, { Component } from 'react';
+import data from './profile.json';
 
 class Profile extends Component {
-
+  constructor(props) {
+    super(props)
+    this.state = data;
+  }
   render() {
     return (
       <section id="profile">
         <div className="container profile">
           <div className="profile-head">
             <h2>Profile</h2>
-            <p>I am a passionate Front-end developer</p>
+            <p>{this.state.profileTitle}</p>
             <div className="split-line split-line-light flex-1-1"></div>
           </div>
           <div className="flex">
             <article className="profile-about flex-1-3">
               <h4>About me</h4>
-              <p>
-                I am a front-end engineer with 3 years of experience in web
-                development, and I have used AngularJS and ReactJS to build web
-                applications for more than 2 years. I also learned Python, Docker
-                and other programming tools by myself and use it in the companys
-                side projects.
-              </p>
+              <p>{this.state.aboutMeContent}</p>
             </article>
             <div className="profile-box flex-1-3">
               <div>
-                <img className="profile-box-image" src="./assets/me.jpg"></img>
+                <img className="profile-box-image" src="../../assets/me.jpg"></img>
               </div>
             </div>
             <article className="profile-details flex-1-3">
               <h4>Details</h4>
               <div className="profile-details-item">
                 <h5>Name: </h5>
-                <p>Edward Shih</p>
+                <p>{this.state.name}</p>
               </div>
               <div className="profile-details-item">
                 <h5>Age: </h5>
-                <p>28</p>
+                <p>{this.state.age}</p>
               </div>
               <div className="profile-details-item">
                 <h5>Location: </h5>
-                <p>Taipei, Taiwan</p>
+                <p>{this.state.location}</p>
               </div>
             </article>
           </div>
