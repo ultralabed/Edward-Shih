32180d3be83b9cb1fae80309a915d1aea56292f2
Ed <ultralabed@gmail.com>
2017-07-27 20:45:12 +0800

    feat: Ability data from ability.json

--- src/components/resume/Ability.js
+++ src/components/resume/Ability.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
+import data from './ability.json';
 
 class Ability extends Component {
+  constructor(props) {
+    super(props)
+    this.state = data;
+  }
+
+  renderLeftList(skills, listDirection) {
+    let list = skills.map((data) => {
+      let stars = data.stars;
+      let starList = []
+      let unStarList = []
+      for (let i=5; i>0;i--) {
+        if (stars > 0) {
+          starList.push((<span className="glyphicon glyphicon-star filled" key={`${data.skill}${i}`}></span>))
+        } else {
+          unStarList.push((<span className="glyphicon glyphicon-star"></span>))
+        }
+        stars--;
+      }
+      starList.push(unStarList)
+      return (
+        <li className={listDirection} key={data.skill}>
+          <span className="ability-title">{data.skill}</span>
+          <span className="ability-score">
+            {starList}
+          </span>
+        </li>
+      )
+    });
+    return [list]
+  }
 
   render() {
     return (
@@ -8,37 +39,48 @@ class Ability extends Component {
         <div className="container abilites">
           <div className="ability-head">
             <h2>Abilities</h2>
-            <p>With the power of force, I am invincible.</p>
+            <p>With the power of the force, I am invincible.</p>
             <div className="split-line split-line-light flex-1-1"></div>
           </div>
           <section id="skills">
             <div className="ability-skills-head">
-              <h2>Skills</h2>
+              <h2>Fronend</h2>
             </div>
             <div className="flex">
               <ul className="flex-1-2 no-bullets">
-                <li className="list-left">
-          				<span className="ability-title">AngularJS</span>
-          				<span className="ability-score">
-          					<span className="glyphicon glyphicon-star filled"></span>
-          					<span className="glyphicon glyphicon-star filled"></span>
-          					<span className="glyphicon glyphicon-star filled"></span>
-          					<span className="glyphicon glyphicon-star filled"></span>
-          					<span className="glyphicon glyphicon-star filled"></span>
-          				</span>
-          			</li>
+                {
+                  this.renderLeftList(
+                  this.state.frontend.splice(0, Math.ceil(this.state.frontend.length / 2)),
+                  'list-left')
+                }
               </ul>
               <ul className="flex-1-2 no-bullets">
-                <li className="list-right">
-                  <span className="ability-title">ReactJS</span>
-                  <span className="ability-score">
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                  </span>
-                </li>
+                {
+                  this.renderLeftList(
+                  this.state.frontend.splice(Math.ceil(this.state.frontend.length / 2)-1, this.state.frontend.length),
+                  'list-right')
+                }
+              </ul>
+            </div>
+          </section>
+          <section id="backend">
+            <div className="ability-others-head">
+              <h2>Backend</h2>
+            </div>
+            <div className="flex">
+              <ul className="flex-1-2 no-bullets">
+                {
+                  this.renderLeftList(
+                  this.state.backend.splice(0, Math.ceil(this.state.backend.length / 2)),
+                  'list-left')
+                }
+              </ul>
+              <ul className="flex-1-2 no-bullets">
+                {
+                  this.renderLeftList(
+                  this.state.backend.splice(Math.ceil(this.state.backend.length / 2)-1, this.state.backend.length),
+                  'list-right')
+                }
               </ul>
             </div>
           </section>
@@ -48,38 +90,18 @@ class Ability extends Component {
             </div>
             <div className="flex">
               <ul className="flex-1-2 no-bullets">
-                <li className="list-left">
-                  <span className="ability-title">MySQL</span>
-                  <span className="ability-score">
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                  </span>
-                </li>
-                <li className="list-left">
-                  <span className="ability-title">NodeJS</span>
-                  <span className="ability-score">
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                  </span>
-                </li>
+                {
+                  this.renderLeftList(
+                  this.state.others.splice(0, Math.ceil(this.state.others.length / 2)),
+                  'list-left')
+                }
               </ul>
               <ul className="flex-1-2 no-bullets">
-                <li className="list-right">
-                  <span className="ability-title">PHP</span>
-                  <span className="ability-score">
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                  </span>
-                </li>
+                {
+                  this.renderLeftList(
+                  this.state.others.splice(Math.ceil(this.state.others.length / 2)-1, this.state.others.length),
+                  'list-right')
+                }
               </ul>
             </div>
           </section>
@@ -89,38 +111,18 @@ class Ability extends Component {
             </div>
             <div className="flex">
               <ul className="flex-1-2 no-bullets">
-                <li className="list-left">
-                  <span className="ability-title">Chinese (Native Speaker)</span>
-                  <span className="ability-score">
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                  </span>
-                </li>
-                <li className="list-left">
-                  <span className="ability-title">Japanese (Can servive)</span>
-                  <span className="ability-score">
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                  </span>
-                </li>
+                {
+                  this.renderLeftList(
+                  this.state.languages.splice(0, Math.ceil(this.state.languages.length / 2)),
+                  'list-left')
+                }
               </ul>
               <ul className="flex-1-2 no-bullets">
-                <li className="list-right">
-                  <span className="ability-title">English(Fluent)</span>
-                  <span className="ability-score">
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                    <span className="glyphicon glyphicon-star filled"></span>
-                  </span>
-                </li>
+                {
+                  this.renderLeftList(
+                  this.state.languages.splice(Math.ceil(this.state.languages.length / 2)-1, this.state.languages.length),
+                  'list-right')
+                }
               </ul>
             </div>
           </section>
