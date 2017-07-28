dfffd08c10eae60b94d395279115755489bbc3c2
Ed <ultralabed@gmail.com>
2017-07-29 00:10:42 +0800

    Enhance: footer layout

--- src/components/resume/Footer.js
+++ src/components/resume/Footer.js
@@ -7,31 +7,37 @@ class Footer extends Component {
       <section id="footer">
         <div className="footer-bk">
           <div className="container footer">
-            <h1>Contact</h1>
+            <h2>Contact</h2>
             <div className="split-line split-line-light"></div>
             <div className="flex footer-link">
-            	<div className="flex-1-2">
+            	<div className="col-1-2">
             		<ul className="no-bullets">
             			<li>
             				<a href="https://github.com/ultralabed" target="_blank">
             					<span className="icon icon-github"></span>
-            					<span>     https://github.com/ultralabed</span>
+            					<span>https://github.com/ultralabed</span>
                     </a>
             			</li>
                   <li>
                     <a href="mailto:ultralabed@gmail.com">
                       <i className="icon icon-mail"></i>
-                      <span>     ultralabed@gmail.com					</span>
+                      <span>ultralabed@gmail.com</span>
                     </a>
                   </li>
             		</ul>
             	</div>
-            	<div className="flex-1-2">
+            	<div className="col-1-2">
             		<ul className="no-bullets">
                   <li>
-                    <a href="https://www.linkedin.com/in/edward-shih-43942786/" target="_blank">
+                    <a href="https://goo.gl/MA91nQ" target="_blank">
                       <span className="icon icon-linkedin"></span>
-                      <span>     https://www.linkedin.com/in/edward-shih-43942786/</span>
+                      <span>https://goo.gl/MA91nQ</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="tel:+886912834980" target="_blank">
+                      <span className="icon icon-phone"></span>
+                      <span>+886 912834980</span>
                     </a>
                   </li>
             		</ul>
